2516a440cc8d68bc925c57828aa0c52c2a846dc8
Arian <142532503+MiArianM@users.noreply.github.com>
2023-08-30 13:31:46 +0330

    index.js

--- Me/index.js
+++ Me/index.js
@@ -1,19 +1,26 @@
 var mobileMenuIcon = document.querySelector(".mobile-menu-icon");
 var mobileMenu = document.querySelector(".group-icons ul");
+var closemenu = document.querySelector(".close-menu");
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("active");
     mobileMenuIcon.classList.toggle("active");
+    closemenu.classList.toggle("active");
 }
 
 function closeMobileMenu() {
     mobileMenu.classList.remove("active");
     mobileMenuIcon.classList.remove("active");
+    closemenu.classList.remove("active");
 }
 
 // Close the menu when clicking outside of it
 document.addEventListener("click", function (event) {
-    if (!mobileMenu.contains(event.target) && !mobileMenuIcon.contains(event.target)) {
+    if (!mobileMenu.contains(event.target) && !closemenu.contains(event.target)) {
         closeMobileMenu();
     }
 });
+
+// Toggle the menu when clicking the hamburger menu or the close button
+closemenu.addEventListener("click", closeMobileMenu);
+mobileMenuIcon.addEventListener("click", toggleMobileMenu);
